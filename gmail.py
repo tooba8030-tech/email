115c4dc18ca93eb83efbbf3d2f84c2d2b0b5660c
toobaanwar240 <148627259+toobaanwar240@users.noreply.github.com>
2025-11-11 14:48:03 +0500

    Update Gmail API scopes to read and send emails

--- gmail.py
+++ gmail.py
@@ -20,7 +20,8 @@ SCOPES = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
     "https://www.googleapis.com/auth/calendar",
     "https://www.googleapis.com/auth/calendar.events",
 ]
